b354bfb09f31bd4eec552ed42d72d7c8a8c3c046
santhosh <santhosh.kumar@thoughtworks.com>
2019-11-25 16:12:18 +0530

    added test cases for saveTransactions.js

---
+++ test/testSaveTransactions.js
@@ -0,0 +1,34 @@
+const saveBeverageTransaction = require("../src/saveTransactions")
+  .saveBeverageTransaction;
+const assert = require("assert");
+const fs = require("fs");
+
+describe("saveBeverageTransactionss", function() {
+  it("should return transaction recorded message", function() {
+    const path = "../beverageTransactions.json";
+    const args = ["--beverage", "Apple", "--empId", "12345", "--qty", "1"];
+    let expected = "Transaction Recorded \nEmployee ID,Beverage,Quantity,time";
+    expected =
+      expected +
+      "\n12345,Apple,1,Mon Nov 25 2019 15:53:13 GMT+0530 (India Standard Time)";
+    const readFile = function(path, typeOfFile) {
+      return fs.readFileSync(path, typeOfFile);
+    };
+
+    const writeFile = function(path, writingContents, typeOfFile) {
+      return fs.writeFileSync(path, writingContents, typeOfFile);
+    };
+
+    const existFile = function(path) {
+      return fs.existsSync(path);
+    };
+
+    const date = function() {
+      return "Mon Nov 25 2019 15:53:13 GMT+0530 (India Standard Time)";
+    };
+    assert.strictEqual(
+      saveBeverageTransaction(args, path, readFile, writeFile, existFile, date),
+      expected
+    );
+  });
+});
